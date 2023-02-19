5aae215263d5a0a9b16825a31ecc2d498d2824df
enpitsulin <enpitsulin@gmail.com>
2023-02-19 16:27:26 +0800

    chore: remove unnecessary test dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,42 @@
 import { defineConfig } from 'vitest/config'
-import solid from 'vite-plugin-solid'
+import solidPlugin from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [solid()],
-  test: {
-    deps: {
-      registerNodeLoader: true,
-      inline: [/solid-js/],
+export default defineConfig(({ mode }) => {
+  // to test in server environment, run with "--mode ssr" or "--mode test:ssr" flag
+  // loads only server.test.ts file
+  const testSSR = mode === 'test:ssr' || mode === 'ssr'
+
+  return {
+    plugins: [
+      solidPlugin({
+        // https://github.com/solidjs/solid-refresh/issues/29
+        hot: false,
+        // For testing SSR we need to do a SSR JSX transform
+        solid: { generate: testSSR ? 'ssr' : 'dom' },
+      }),
+    ],
+    test: {
+      watch: false,
+      isolate: !testSSR,
+      env: {
+        NODE_ENV: testSSR ? 'production' : 'development',
+        DEV: testSSR ? '' : '1',
+        SSR: testSSR ? '1' : '',
+        PROD: testSSR ? '1' : '',
+      },
+      environment: testSSR ? 'node' : 'jsdom',
+      transformMode: { web: [/\.[jt]sx$/] },
+      ...(testSSR
+        ? {
+            include: ['test/server.spec.{ts,tsx}'],
+          }
+        : {
+            include: ['test/*.spec.{ts,tsx}'],
+            exclude: ['test/server.spec.{ts,tsx}'],
+          }),
     },
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect', './setupVitest.js'],
-    transformMode: { web: [/\.[jt]sx?$/] },
-    coverage: {
-      provider: 'c8',
-      reporter: ['json', 'json-summary', 'text'],
-      lines: 100,
-      functions: 100,
-      branches: 100,
-      statements: 100,
+    resolve: {
+      conditions: testSSR ? ['node'] : ['browser', 'development'],
     },
-  },
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
+  }
 })
